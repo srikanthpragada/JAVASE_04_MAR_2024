3209ed27fc58248fa3f6fe5826516a08e7aa49dd
srikanthpragada <srikanthpragada@gmail.com>
2024-03-07 18:52:38 +0530

    07-MAR

---
+++ demo/src/basics/TextBlockDemo.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class TextBlockDemo {
+
+	public static void main(String[] args) {
+		 String s = "This is a string";
+		 
+		 String s2 = """
+		 		This is first line
+		 		second line
+		 		thrid line 
+		 		""";
+		 
+		 
+
+	}
+
+}
