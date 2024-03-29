041789cda8f3c099955c58e1b049aa10faa67ced
srikanthpragada <srikanthpragada@gmail.com>
2024-03-29 18:50:56 +0530

    29-MAR

--- demo/src/oop2/ExDemo.java
+++ demo/src/oop2/ExDemo.java
@@ -3,10 +3,24 @@ package oop2;
 public class ExDemo {
 
 	public static void main(String[] args) {
-		 int a = 10, b = 0;
-		 
-		 System.out.println(a / b);
-		 
+		int a = 10;
+		String s = "0";
+
+		try {
+			int b = Integer.parseInt(s);
+			int c = a / b;
+			System.out.println(c);
+		} catch (ArithmeticException ex) {
+			System.out.println("Sorry! Zero is not valid!");
+		}
+//		catch(NumberFormatException ex) {
+//			System.out.println("Invalid Number!");
+//		}
+		finally {
+			System.out.println("Finally!");
+		}
+
+		System.out.println("The End!");
 	}
 
 }
